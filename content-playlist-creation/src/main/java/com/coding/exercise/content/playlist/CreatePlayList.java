99a0fd25aff26e4fef0da77f91a183699f43ad1e
Shwetha Vasanth <shwethav29@gmail.com>
2019-02-02 09:13:09 -0800

    log4j confguration

--- content-playlist-creation/src/main/java/com/coding/exercise/content/playlist/CreatePlayList.java
+++ content-playlist-creation/src/main/java/com/coding/exercise/content/playlist/CreatePlayList.java
@@ -1,6 +1,11 @@
 package com.coding.exercise.content.playlist;
 
+import static com.google.common.base.Preconditions.*;
+
 import java.io.Reader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.coding.exercise.content.playlist.creation.model.PlayListData;
 /**
@@ -11,12 +16,17 @@ import com.coding.exercise.content.playlist.creation.model.PlayListData;
  */
 public final class CreatePlayList {
 	
+	public static final Logger logger = LogManager.getLogger(CreatePlayList.class);
 	/**
 	 * Return the {@link PlayListData} by parsing the data from the reader using the appropriate parser based on the {@link DataFormat}
 	 */
+
 	public PlayListData loadPlayListData(Reader reader,DataFormat dataFormat) {
+		checkNotNull(reader, "Reader was null");
+		checkNotNull(dataFormat,"Data format null");
 		PlayListDataParserFactory playListDataParserFactory = new PlayListDataParserFactory();
 		PlayListDataParser parser = playListDataParserFactory.createPlayListDataParser(dataFormat);
+		logger.debug("Play list data was parser from the input");
 		return parser.parsePlayListData(reader);
 	}
 	/**
